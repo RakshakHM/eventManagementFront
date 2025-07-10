a66815d6abeb452fed01e285bdd41afbd04f2485
RakshakHM <raakshak@gmail.com>
2025-07-10 17:33:15 +0530

    vercel deploy

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,7 +32,7 @@ export const getImageUrl = (imagePath?: string) => {
   if (imagePath.startsWith("data:")) return imagePath;
   if (imagePath.startsWith("http")) return imagePath;
   const baseUrl = process.env.NODE_ENV === 'production'
-    ? "https://eventmanagementapi-ej02.onrender.com"
+    ? "https://event-management-api-ebon.vercel.app/"
     : "http://localhost:3001";
   return baseUrl + imagePath;
 };
